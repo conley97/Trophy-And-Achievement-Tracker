9061819a23256949da79b4bad914ffc3b320a169
conley97 <62983528+conley97@users.noreply.github.com>
2024-07-09 00:43:23 -0400

    Delete backend directory

--- backend/dist/routes/steamRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const steamController_1 = require("../controllers/steamController");
-const router = (0, express_1.Router)();
-router.get('/:userId/achievements', steamController_1.getUserAchievements);
-exports.default = router;
