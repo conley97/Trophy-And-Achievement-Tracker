a880b08cfca557423582ba0437478a98d691bbe8
conley97 <62983528+conley97@users.noreply.github.com>
2024-07-09 00:19:43 -0400

    Delete backend directory

--- backend/src/routes/steamRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getUserAchievements } from '../controllers/steamController';
-
-const router = Router();
-
-router.get('/:userId/achievements', getUserAchievements);
-
-export default router;
--- backend/src/routes/xboxRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getUserXboxAchievements } from '../controllers/xboxController';
-
-const router = Router();
-
-router.get('/:userId/achievements', getUserXboxAchievements);
-
-export default router;
